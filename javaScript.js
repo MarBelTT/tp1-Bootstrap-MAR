010b03d31447635755e964320842512054cf1e7d
Marisol <maris_bel17@hotmail.com>
2022-11-13 23:47:17 -0300

    agregado Javascript

---
+++ javaScript.js
@@ -0,0 +1,23 @@
+
+
+function captura(){
+    var nombre= document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var direcEmail = document.getElementById("email").value;
+    var cant = document.getElementById("cantidad").value;
+    
+    
+}
+
+function valorApagar(){
+    const PrecioEntrada = 200;
+    var pagar = PrecioEntrada * cant ;
+    var descuento = pagar * 
+}
+
+function borrar(){
+    document.getElementById("nombre").value= " ";
+    document.getElementById("apellido").value = " ";
+    document.getElementById("email").value = " ";
+    document.getElementById("cantidad").value = " ";
+}
